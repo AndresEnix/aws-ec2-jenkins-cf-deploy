b4d9cf5a1810f2dc79604ed4960a859317aa1e90
AndresEnix <andres.enix@gmail.com>
2019-05-14 22:11:46 -0500

    Creates bucket with version configuration enabled

--- run.sh
+++ run.sh
@@ -1,27 +1,29 @@
 #!/bin/bash -e
 
-# Attributes
 BRANCH_NAME=''
 COMMIT_HASH=''
-CFT_BUCKET='ae-cloudformation-templates'
-PEM_BUCKET='ae-key-pairs'
+CFT_BUCKET=''
 
-# $1 = Branch name
 checkout() {
     git pull
     git checkout $1
     BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
     COMMIT_HASH=$(git rev-parse --verify HEAD)
+    CFT_BUCKET="ae-$BRANCH_NAME-environment-templates"
 }
 
-# $1 = S3 bucket
-# $2 = Local folder
-# $3 = Display name
 synch_files() {
-    echo "Sync $3 with S3 bucket"
+    if aws s3 ls "s3://$1" 2>&1 | grep -q 'NoSuchBucket' ;
+    then
+        echo "Creating $1 bucket with versioning configuration enabled"
+        aws s3api create-bucket --bucket $1 --region us-east-1
+        aws s3api put-bucket-versioning --bucket $1 --versioning-configuration Status=Enabled
+    fi
+
+    echo "Uploading $3 to $1 bucket"
     aws s3 sync $2 s3://$1/
 
-    echo "Grant public access to $3"
+    echo "Grant public access to templates inside $3 bucket"
     for file in $2/* 
     do
         aws s3api put-object-acl --bucket $1 --key $(basename $file) --acl public-read
@@ -32,21 +34,7 @@ run_playbook(){
     ansible-playbook deploy-environment.yml --extra-vars "branch_name=$1 commit_hash=$2 cf_bucket=$3"
 }
 
-# $1 = S3 bucket
-# $2 = Local folder
-download_pems(){
-    mkdir -p -m 777 $2
-    aws s3 sync s3://$1 $2
-    find . -name '*.pem' -exec chmod 400 {} \;
-}
-
-clean_environment(){
-    rm -rf $1
-}
-
 # Body
 checkout $BRANCH_NAME
 synch_files $CFT_BUCKET "cloudformation_templates" "cloudformation templates"
 run_playbook $BRANCH_NAME $COMMIT_HASH $CFT_BUCKET
-download_pems $PEM_BUCKET "key_pairs"
-clean_environment "key_pairs"
